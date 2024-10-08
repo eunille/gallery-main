3558a59b9f7f7329bec2f8c113cecafc1a05ca6a
Eunille Jan Apo <beerusgaming24@gmail.com>
2024-10-08 08:39:13 +0800

    second commit

--- src/app/lightbox/lightbox.component.ts
+++ src/app/lightbox/lightbox.component.ts
@@ -28,8 +28,7 @@ interface Comment {
 @Component({
   selector: 'app-lightbox',
   templateUrl: './lightbox.component.html',
-  styleUrls: ['./lightbox.component.scss'],
-  
+  styleUrls: ['./lightbox.component.css'],
 })
 export class LightboxComponent {
   @Input() selectedImage: any;
